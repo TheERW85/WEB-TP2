31819e6019ecf74d0dce9b45d8844f2ac25d9f36
theerw85 <theerw85@live.fr>
2020-04-29 10:20:27 +0200

    tp2.2.1 done

---
+++ tp2.2.1.js
@@ -0,0 +1,17 @@
+/**
+ * @author Erwan LAUNAY
+ */
+'use strict';
+
+const albums = {
+    'Fresh Cream': 'Cream',
+    'Hot Rats': 'Frank Zappa',
+    'Space Oddity': 'David Bowie',
+    'Merry Christmas': 'Mariah Carey',
+    'Songs from a Room': 'Leonard Cohen',
+    'Ummagumma': 'Pink Floyd',
+    'Camembert Électrique': 'Gong',
+    'The Piper at the Gates of Dawn': 'Pink Floyd',
+};
+
+console.log(`${albums['Fresh Cream']} === Cream`);
